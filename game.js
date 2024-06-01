be12f4d633baaf9bd8944ce9b110a32a3fd10475
mud-master <ctvonkleeck@gmail.com>
2024-06-01 11:26:00 -0400

    Clean console output

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ console.log("Let's play Rock, Paper, Scissors!");
 
 let humanScore = 0;
 let computerScore = 0;
-let round = 0;
+let round = 1;
 
 function getComputerChoice() {
   let random_num = Math.random();
@@ -17,47 +17,77 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    while(true) {
-        let human_choice = prompt("Enter your choice: ");
+  while (true) {
+    let human_choice = prompt("Enter your choice: ");
 
-        if (human_choice.toLowerCase() == "rock" || human_choice.toLowerCase() == "paper" || human_choice.toLowerCase() == "scissors") {
-            return human_choice.toLowerCase();
-        }
+    if (
+      human_choice.toLowerCase() == "rock" ||
+      human_choice.toLowerCase() == "paper" ||
+      human_choice.toLowerCase() == "scissors"
+    ) {
+      return human_choice.toLowerCase();
     }
+  }
 }
 
-
 function playRound() {
-    let computerChoice = getComputerChoice();
-    let humanChoice = getHumanChoice();
+  let computerChoice = getComputerChoice();
+  let humanChoice = getHumanChoice();
 
-    console.log("You chose " + humanChoice + " and the computer chose " + computerChoice + "!")
+  if (
+    (computerChoice == "rock" && humanChoice == "paper") ||
+    (computerChoice == "paper" && humanChoice == "scissors") ||
+    (computerChoice == "scissors" && humanChoice == "rock")
+  ) {
+    humanScore++;
 
-    if ((computerChoice == "rock" && humanChoice == "paper") || (computerChoice == "paper" && humanChoice == "scissors") || (computerChoice == "scissors" && humanChoice == "rock")) {
-        humanScore++;
-        console.log("You win! " + humanChoice.toUpperCase().charAt(0) + humanChoice.substring(1) + " beats " + computerChoice);
-    } else if (computerChoice == humanChoice) {
-        console.log("You tied! You both chose " + humanChoice)
-    } else {
-        computerScore++;
-        console.log("You lose! " +  computerChoice.toUpperCase().charAt(0) + computerChoice.substring(1) + " beats " + humanChoice);
-    }
-
-    console.log("Current score: You: " + humanScore + " Computer: " + computerScore);
+    console.log(
+      "Round " +
+        round +
+        ": You win! " +
+        humanChoice.toUpperCase().charAt(0) +
+        humanChoice.substring(1) +
+        " beats " +
+        computerChoice + " | Current score: You: " + humanScore + " Computer: " + computerScore
+    );
     round++;
+  } else if (computerChoice == humanChoice) {
+    console.log(
+        "Round " + round
+         + ": You both chose " + humanChoice + "! Let's replay round " + round
+    );
+  } else {
+    computerScore++;
+
+    console.log(
+        "Round " +
+          round +
+          ": You lose! " +
+          computerChoice.toUpperCase().charAt(0) +
+          computerChoice.substring(1) +
+          " beats " +
+          humanChoice + " | Current score: You: " + humanScore + " Computer: " + computerScore
+      );
+
+      round++;
+  }
 }
 
 function playGame() {
+  while (round <= 5) {
+    if (playRound()) {
+      round++;
+    }
+  }
 
-    while (round < 5) {
-        playRound();
-    }
-    
-    if (humanScore > computerScore) {
-        console.log("Congrats! You win the game! 🦧")
-    } else {
-        console.log("Game over. You lose!");
-    }
+  console.log(
+    "Final score: You: " + humanScore + " Computer: " + computerScore
+  );
+  if (humanScore > computerScore) {
+    console.log("Congrats! You win the game! 🦧");
+  } else {
+    console.log("Game over. You lose!");
+  }
 }
 
 playGame();
